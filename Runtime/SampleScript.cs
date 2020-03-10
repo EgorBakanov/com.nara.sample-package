b90ec2307a53501e330820f3458115efc4cedd30
Egor Bakanov <egorbakanov@gmail.com>
2020-03-10 13:52:22 +0700

    [0.4.0] Sample Script
    Add SampleScript file

---
+++ Runtime/SampleScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Nara.SamplePackage
+{
+    public static class SampleScript
+    {
+#if UNITY_EDITOR
+        [MenuItem("Sample script/Run")]
+#endif
+        public static void Run()
+        {
+            Debug.Log("Running sample script...");
+        }
+    }
+}
